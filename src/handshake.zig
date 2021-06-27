059e2a70aba432db802bbc6b19eabd5d38dd3b56
Luuk de Gram <Luukdegram@users.noreply.github.com>
2021-06-27 21:31:20 +0200

    Fix compilation errors. Ensure endianness. Add a test case

--- src/handshake.zig
+++ src/handshake.zig
@@ -24,6 +24,10 @@ pub const HandshakeType = enum(u8) {
 pub const DecodeError = error{
     /// For TLS 1.3, the legacy version must be 0x0303 (TLS 1.2)
     MismatchingLegacyVersion,
+    /// Reached end of stream, perhaps the client disconnected.
+    EndOfStream,
+    /// The client requested for an extension the server does not support.
+    UnsupportedExtension,
 };
 
 /// Initializes a new reader that decodes and performs a handshake
@@ -46,16 +50,16 @@ pub fn HandshakeReader(comptime ReaderType: type) type {
         /// Initializes a new instance of `HandshakeReader` of a given reader that must be of
         /// `ReaderType`.
         pub fn init(gpa: *Allocator, reader: ReaderType) Self {
-            return .{ .gpa = gpa, .handshake = undefined, .reader = reader };
+            return .{ .gpa = gpa, .reader = reader };
         }
 
         /// Starts reading from the reader and will try to perform a handshake.
         pub fn decode(self: *Self) Error!void {
             const handshake_type = try self.reader.readByte();
-            const remaining_length = try self.reader.ReadIntBig(u24);
+            const remaining_length = try self.reader.readIntBig(u24);
 
             switch (@intToEnum(HandshakeType, handshake_type)) {
-                .client_hello => try self.decodeClientHello(reader, remaining_length),
+                .client_hello => try self.decodeClientHello(remaining_length),
                 else => @panic("TODO"),
             }
         }
@@ -64,8 +68,8 @@ pub fn HandshakeReader(comptime ReaderType: type) type {
         fn decodeClientHello(self: *Self, message_length: usize) Error!void {
             // maximum length of client hello
             var buf: [2 + 32 + 32 + (2 ^ 16 - 2) + (2 ^ 8 - 1) + (2 ^ 16 - 1)]u8 = undefined;
-            try reader.readNoEof(buf[0..message_length]);
-            const content = buf[0..self.length];
+            try self.reader.readNoEof(buf[0..message_length]);
+            const content = buf[0..message_length];
             // current index into `contents`
             if (std.mem.readIntBig(u16, content[0..2]) != 0x0303) return error.MismatchingLegacyVersion;
             var index: usize = 2;
@@ -83,7 +87,7 @@ pub fn HandshakeReader(comptime ReaderType: type) type {
                 _ = session_id;
             }
 
-            const cipher_suites_len = std.mem.readIntBig(u16, content[index .. index + 2]);
+            const cipher_suites_len = std.mem.readIntBig(u16, content[index..][0..2]);
             index += 2;
             const cipher_suites = content[index .. index + cipher_suites_len];
             _ = cipher_suites; //TODO, check this
@@ -110,10 +114,13 @@ pub fn HandshakeReader(comptime ReaderType: type) type {
         }
 
         const ExtensionIterator = struct {
-            data: []const u8,
+            /// Mutable slice as we may require
+            /// to byteswap elements to ensure correct endianness
+            data: []u8,
+            /// Current index into `data`
             index: usize,
 
-            fn next(self: *ExtensionIterator, gpa: *Allocator) error{OutOfMemory}!?Extension {
+            fn next(self: *ExtensionIterator, gpa: *Allocator) error{ OutOfMemory, UnsupportedExtension }!?Extension {
                 if (self.index >= self.data.len) return null;
 
                 const tag_byte = std.mem.readIntBig(u16, self.data[self.index..][0..2]);
@@ -126,14 +133,17 @@ pub fn HandshakeReader(comptime ReaderType: type) type {
                 switch (@intToEnum(Extension.Tag, tag_byte)) {
                     .supported_versions => {
                         const versions = blk: {
-                            var versions = std.mem.bytesAsSlice(u16, extension_data[1..]);
-                            if (target_endianness == .Little) @byteSwap(u16, &versions);
+                            var versions = @alignCast(2, std.mem.bytesAsSlice(u16, extension_data[1..]));
+                            if (target_endianness == .Little) for (versions) |*v| {
+                                v.* = @byteSwap(u16, v.*);
+                            };
+
                             break :blk versions;
                         };
 
                         return Extension{ .supported_versions = versions };
                     },
-                    .exchange_modes => return Extension{ .exchange_modes = extension_data[1..] },
+                    .psk_key_exchange_modes => return Extension{ .psk_key_exchange_modes = extension_data[1..] },
                     .key_share => {
                         const len = extension_data[0];
                         var keys = std.ArrayList(KeyShare).init(gpa);
@@ -153,27 +163,62 @@ pub fn HandshakeReader(comptime ReaderType: type) type {
 
                         return Extension{ .key_share = keys.toOwnedSlice() };
                     },
+                    // For TLS 1.3 only 1 hostname can be provided which is always
+                    // of type DNS hostname. This means the hostname is the remaining
+                    // bytes after the 5th element.
+                    .server_name => return Extension{ .server_name = extension_data[5..] },
+                    .supported_groups => {
+                        var groups = @alignCast(2, std.mem.bytesAsSlice(NamedGroup, extension_data[1..]));
+                        if (target_endianness == .Little) for (groups) |*group| {
+                            group.* = @intToEnum(NamedGroup, @byteSwap(u16, @enumToInt(group.*)));
+                        };
+                        return Extension{ .supported_groups = groups };
+                    },
+                    .signature_algorithms => {
+                        var algs = @alignCast(2, std.mem.bytesAsSlice(SignatureAlgorithm, extension_data[2..]));
+                        if (target_endianness == .Little) for (algs) |*alg| {
+                            alg.* = @intToEnum(SignatureAlgorithm, @byteSwap(u16, @enumToInt(alg.*)));
+                        };
+                        return Extension{ .signature_algorithms = algs };
+                    },
+                    else => return error.UnsupportedExtension,
                 }
             }
         };
 
+        /// Extension define what the client requests for extended functionality from servers.
+        /// Note that some extensions are required for TLS 1.3 itself,
+        /// while others are optional.
         const Extension = union(Tag) {
+            /// The supported TLS versions of the client.
             supported_versions: []const u16,
             /// The PSK key exchange modes the client supports
-            exchange_modes: []const u8,
+            psk_key_exchange_modes: []const u8,
+            /// List of public keys and the key exchange required for them.
             key_share: []const KeyShare,
             /// A list of signature algorithms the client supports
-            signature_alg: []const u16,
+            signature_algorithms: []const SignatureAlgorithm,
             /// The groups of curve types the client supports
             supported_groups: []const NamedGroup,
+            /// Hostname of the server the client wants to connect to
+            /// TLS uses this to determine which server certificate to use,
+            /// rather than requiring multiple servers.
             server_name: []const u8,
-
-            const KeyShare = struct {
-                /// The key exchange (i.e. curve25519)
-                named_group: NamedGroup,
-                /// The public key of the client
-                key_exchange: []const u8,
-            };
+            max_gragment_length,
+            status_request,
+            use_srtp,
+            heartbeat,
+            application_layer_protocol_negotation,
+            signed_certificate_timestamp,
+            client_certificate_type,
+            server_certificate_type,
+            pre_shared_key,
+            early_data,
+            cookie,
+            certificate_authorities,
+            oid_filters,
+            post_handshake_auth,
+            signature_algorithms_cert,
 
             /// All extensions that are compatible with TLS 1.3
             /// Some may be specified as an external rfc.
@@ -189,7 +234,6 @@ pub fn HandshakeReader(comptime ReaderType: type) type {
                 signed_certificate_timestamp = 18,
                 client_certificate_type = 19,
                 server_certificate_type = 20,
-                _padding = 21,
                 pre_shared_key = 41,
                 early_data = 42,
                 supported_versions = 43,
@@ -224,3 +268,103 @@ const NamedGroup = enum(u16) {
     /// as they're part of earlier TLS version.
     _,
 };
+
+const SignatureAlgorithm = enum(u16) {
+    // RSASSA-PKCS1-v1_5 algorithms
+    rsa_pkcs1_sha256 = 0x0401,
+    rsa_pkcs1_sha384 = 0x0501,
+    rsa_pkcs1_sha512 = 0x0601,
+
+    // ECDSA algorithms
+    ecdsa_secp256r1_sha256 = 0x0403,
+    ecdsa_secp384r1_sha384 = 0x0503,
+    ecdsa_secp521r1_sha512 = 0x0603,
+
+    // RSASSA-PSS algorithms with public key OID rsaEncryption
+    rsa_pss_rsae_sha256 = 0x0804,
+    rsa_pss_rsae_sha384 = 0x0805,
+    rsa_pss_rsae_sha512 = 0x0806,
+
+    // EdDSA algorithms
+    ed25519 = 0x0807,
+    ed448 = 0x0808,
+
+    // RSASSA-PSS algorithms with public key OID RSASSA-PSS
+    rsa_pss_pss_sha256 = 0x0809,
+    rsa_pss_pss_sha384 = 0x080a,
+    rsa_pss_pss_sha512 = 0x080b,
+
+    // Legacy algorithms
+    rsa_pkcs1_sha1 = 0x0201,
+    ecdsa_sha1 = 0x0203,
+
+    /// Reserved Code Points
+    _,
+};
+
+/// Keyshare represents the key exchange used to generate
+/// its public key, and the actual public key.
+const KeyShare = struct {
+    /// The key exchange (i.e. curve25519)
+    named_group: NamedGroup,
+    /// The public key of the client
+    key_exchange: []const u8,
+};
+
+test "Client Hello" {
+    // Client hello bytes taken from:
+    // https://tls13.ulfheim.net/
+
+    // zig fmt: off
+    var data = [_]u8{
+        // Handshake header
+        0x01, 0x00, 0x00, 0xc6,
+        // client version
+        0x03, 0x03,
+        // random
+        0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+        0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+        0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
+        0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f,
+        // Session id
+        0x20, 0xe0, 0xe1, 0xe2, 0xe3, 0xe4, 0xe5, 0xe6,
+        0xe7, 0xe8, 0xe9, 0xea, 0xeb, 0xec, 0xed, 0xee,
+        0xef, 0xf0, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6,
+        0xf7, 0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe,
+        // Cipher suites
+        0xff, 0x00, 0x06, 0x13, 0x01,
+        0x13, 0x02, 0x13, 0x03,
+        // Compression methods
+        0x01, 0x00,
+        // Extension length
+        0x00, 0x77,
+           // Extension - Server name
+        0x00, 0x00, 0x00, 0x18, 0x00, 0x16, 0x00, 0x00,
+        0x13, 0x65, 0x78, 0x61, 0x6d, 0x70, 0x6c, 0x65,
+        0x2e, 0x75, 0x6c, 0x66, 0x68, 0x65, 0x69, 0x6d,
+        0x2e, 0x6e, 0x65, 0x74,
+        // Extension - Support groups
+        0x00, 0x0a, 0x00, 0x08, 0x00, 0x06, 0x00, 0x1d,
+        0x00, 0x17, 0x00, 0x18,
+        // Extension - Signature Algorithms
+        0x00, 0x0d, 0x00, 0x14, 0x00, 0x12, 0x04, 0x03,
+        0x08, 0x04, 0x04, 0x01, 0x05, 0x03, 0x08, 0x05,
+        0x05, 0x01, 0x08, 0x06, 0x06, 0x01, 0x02, 0x01,
+        // Extensions - Key Share
+        0x00, 0x33, 0x00, 0x26, 0x00, 0x24, 0x00, 0x1d,
+        0x00, 0x20, 0x35, 0x80, 0x72, 0xd6, 0x36, 0x58,
+        0x80, 0xd1, 0xae, 0xea, 0x32, 0x9a, 0xdf, 0x91,
+        0x21, 0x38, 0x38, 0x51, 0xed, 0x21, 0xa2, 0x8e,
+        0x3b, 0x75, 0xe9, 0x65, 0xd0, 0xd2, 0xcd, 0x16,
+        0x62, 0x54,
+        // Extension - PSK Key Exchange modes
+        0x00, 0x2d, 0x00, 0x02, 0x01, 0x01,
+        // Extension - Supported versions
+        0x00, 0x2b, 0x00, 0x03, 0x02, 0x03, 0x04,
+    };
+    // zig-fmt: on
+
+    var fb_reader = std.io.fixedBufferStream(&data).reader();
+    var hs_reader = handshakeReader(std.testing.allocator, fb_reader);
+    try hs_reader.decode();
+}
